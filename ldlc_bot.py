4d87cb1145a156b5c244343be6922c9e01b8919c
Claudio Bottari <bottari@gmail.com>
2021-10-02 18:08:14 +0200

    added results

--- ldlc_bot.py
+++ ldlc_bot.py
@@ -1,10 +1,10 @@
 #!/usr/bin/env python3
 __author__ = "Claudio Bottari"
-__version__ = "0.1.0"
+__version__ = "0.2.0"
 __license__ = "MIT"
 
 # target prices for each GPU
-ths = {'3060 ti': 450, '3070 ti': 650, '3070': 550, '3080': 800}
+ths = {'3060 ti': 450, '3070 ti': 650, '3060': 350, '3070': 550, '3080': 800}
 # how many seconds betweens checks   
 sleep_time = 60
 # selenium edge driver path (can run with Chrome or Firefox with few code updates)
@@ -16,7 +16,7 @@ import os
 from time import sleep
 
 from playsound import playsound
-from datetime import datetime
+from datetime import datetime, timedelta
 
 from pathlib import Path
 from selenium import webdriver
@@ -50,8 +50,9 @@ def alarm():
         playsound('siren.wav')
         time.sleep(5)
 
-def current_time():
-    return datetime.now().strftime("%H:%M:%S")
+def get_time_str(s=0):
+    delta = timedelta(0, s)
+    return (datetime.now() + delta).strftime("%H:%M:%S")
 
 def get_data_ldlc(driver):
     driver.get('https://www.ldlc.com/it-it/informatica/componenti/scheda-video/c4684/')
@@ -71,11 +72,12 @@ def get_data_next(driver):
     availabilities = ['NON' not in x.text for x in driver.find_elements_by_xpath('//p[@class="lista-dispo"]/a')]
     return [GPU(x, 'Next') for x in zip(uids, titles, urls, prices, availabilities) if x[4]] #return only the available GPUs
 
-def check_price(driver, data, ths):
+def check_price(driver, data, ths, min_prices):
     for card in data:
         print(card)
         for th in ths:
             if th in card.title:
+                min_prices[th] = min(min_prices[th], card.price)
                 if ths[th] > card.price:
                     print('\n\nTARGET FOUND')
                     print(card)
@@ -88,23 +90,32 @@ def check_price(driver, data, ths):
                 break
 
 def print_header(ths):
-    print('GPU BOT by Claudio\n\nChecking on LDLC for these GPUs:')
-    for th in ths:
-        print(f'\t[{th}] for less than {ths[th]}')
-    print(f'The BOT will keep running until manually stopped, current time: {current_time()}\n\n')
+    print('GPU BOT by Claudio')
+    # for th in ths:
+    #     print(f'\t[{th}] for less than {ths[th]}')
+    print(f'The BOT will keep running until manually stopped, current time: {get_time_str()}\n\n')
+
+def print_footer(ths, min_prices, sleep_time):
+    print('\nResults:')
+    for th in min_prices:
+        price = min_prices[th]
+        price = str(price) + ' €' if price < 99999 else '-'
+        print(f'\t[{th}] target {ths[th]} €, current best price {price}')
+    print(f'\nNext run at: {get_time_str(sleep_time)}\n\n')
 
 def main(driver_path, ths, sleep_time):
     driver = get_driver(driver_path)
+    min_prices = {x: 99999 for x in ths.keys()}
     while True:
         clear_screen()
         print_header(ths)
 
         try:
-            check_price(driver, get_data_ldlc(driver), ths)
-            check_price(driver, get_data_next(driver), ths)
+            check_price(driver, get_data_ldlc(driver), ths, min_prices)
+            check_price(driver, get_data_next(driver), ths, min_prices)
         except Exception as e: print(e)
 
-        print()
+        print_footer(ths, min_prices, sleep_time)
         sleep(sleep_time)
  
 if __name__ == "__main__":
